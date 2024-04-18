0daf76274675e8079c059911e1a5c0a8f82f1c67
L1ttel_Y <L1ttel_Y@126.com>
2024-04-18 12:58:46 +0800

    完成了新的启动界面

--- Assets/Scripts/StepButton.cs
+++ Assets/Scripts/StepButton.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StepButton : MonoBehaviour
-{
-    public GameObject pinch;
-    public GameObject treeBtn;
-    public void UpdateStep()
-    {
-        pinch.SetActive(false);
-        treeBtn.SetActive(true);
-    }
+public class StepButton:MonoBehaviour {
+	public GameObject pinch;
+	public GameObject treeBtn;
+	[SerializeField] GameObject popUpObject;
+	public void UpdateStep() {
+		popUpObject.transform.localScale=Vector3.one;
+		pinch.SetActive(false);
+		treeBtn.SetActive(true);
+	}
+
 }
