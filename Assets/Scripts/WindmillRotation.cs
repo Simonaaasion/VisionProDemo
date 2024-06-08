79429fd80df2cbb9335d2d9827652e4df3f8c997
L1ttel_Y <L1ttel_Y@126.com>
2024-06-09 00:04:17 +0800

    完成了灯塔光照

--- Assets/Scripts/WindmillRotation.cs
+++ Assets/Scripts/WindmillRotation.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class WindmillRotation:MonoBehaviour {
 	[SerializeField] GameObject restartButton;
 	[SerializeField] float rotation = 60;
+	[SerializeField] float rotationZ = 0;
 	[SerializeField] float translation = 0;
+	[SerializeField] bool startInvisible;
+	[SerializeField] Vector3 scale;
 	void Update() {
+		if(startInvisible) transform.localScale=scale;
 		if(!restartButton.activeInHierarchy) return;
-		transform.Rotate(0,Time.deltaTime*rotation,0);
+		transform.Rotate(0,Time.deltaTime*rotation,Time.deltaTime*rotationZ);
 		transform.position+=Vector3.right*translation;
 	}
 }
